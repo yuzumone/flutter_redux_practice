fbce4cb6ee5b41c17163e0055c3b5852ad65f4a4
yuzumone <yuzumone@outlook.com>
2019-09-08 22:23:29 +0900

    Add action

---
+++ lib/action.dart
@@ -0,0 +1,9 @@
+class FetchTdlAttractionListAction {
+  final List<Map<String, String>> attractions;
+  FetchTdlAttractionListAction(this.attractions);
+}
+
+class FetchTdsAttractionListAction {
+  final List<Map<String, String>> attractions;
+  FetchTdsAttractionListAction(this.attractions);
+}
